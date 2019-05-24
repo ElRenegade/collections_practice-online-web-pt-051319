fe8bd1be36381f566b51d2714fa96b428eab39fc
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-24 18:09:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,3 +11,15 @@ def sort_array_desc(array)
     -1
   end}
 end
+
+def sort_array_char_count(array)
+  array.sort{|a, b|
+  if a == b && a.length == b.length
+    0
+  elsif a < b && a.length < b.length
+    -1
+  elsif a > b && a.length > b.length
+    1
+  end}
+
+end
